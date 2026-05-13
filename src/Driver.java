ffbdc336e7f97028e291deb58c3b5d4689ece97d
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-14 00:29:24 +0600

    basic oop code

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main (String[]args) {
+        Pen p1 = new Pen();
+        p1.color = "Red";
+        p1.type = "Gel";
+
+        p1.write();
+
+    }
+}
---
+++ src/Pen.java
@@ -0,0 +1,8 @@
+public class Pen {
+    String color;
+    String type;//ballpoint/gelpen
+
+    public void write () {
+        System.out.println("Writting a Essay");
+    }
+}
